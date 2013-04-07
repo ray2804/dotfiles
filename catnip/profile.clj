0833cb9994bd3c9f40d08c8bd53a62aba1b36e68
supersymmetry <maildumpert@gmail.com>
2013-04-07 09:11:17 +0200

    update

---
+++ catnip/profile.clj
@@ -0,0 +1,2 @@
+{:default-browser-url "/intro.html",
+ :snippets {"testfn" "(with-test\n  (defn )\n  (is ))\n"}}
---
+++ overtone/config.clj
@@ -0,0 +1,5 @@
+{:versions-seen #{"v0.8"},
+ :sc-args {},
+ :server :internal,
+ :user-name "Baal",
+ :os :linux}
